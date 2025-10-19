68dc3b49114a3f9c9282a50881808367fd11332b
Lufford <146119753+Lufford@users.noreply.github.com>
2025-10-19 16:06:42 -0400

    fixed bullets, removed some turrets and unused ladder

--- Midterm/Assets/Scripts/Player/BulletController.cs
+++ Midterm/Assets/Scripts/Player/BulletController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
@@ -10,5 +11,13 @@ public class BulletController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(transform.right * speed);
         Destroy(gameObject, 1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
     }
 }
